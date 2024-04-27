79a39eec7fd53df6cbf322622d11305fb851e7b4
Octavio <octaviorassi@gmail.com>
2024-04-27 16:22:34 -0300

    modified the delete function to correctly handle the case where the key being deleted has children and no centre

--- TTree.hs
+++ TTree.hs
@@ -77,25 +77,63 @@ keyToTTree (x:xs) v = Node x Nothing E (keyToTTree xs v) E
 
 
 -- c) Elimina una clave y el valor asociado a esta en un arbol
+
+-- minAux toma un arbol y devuelve una tupla (TTree, TTree) tal que
+--   el primer  elemento es el par key-value del nodo minimo
+--   el segundo elemento es el arbol del medio de ese subarbol
+--   el tercer  elemento es el arbol original donde se reemplazo ese subarbol minimo por su subarbol derecho
+
+-- es una funcion media rara pero retorna exactamente lo que necesitamos para poder deletear y encontrar su reemplazo
+
+minKeyValTree :: Ord k => TTree k v -> ((k, Maybe v), TTree k v, TTree k v)
+minKeyValTree E                    = error "Error: cannot determine minimum of an empty tree"
+minKeyValTree t@(Leaf k v)         = ((k, Just v), E, E)
+minKeyValTree t@(Node k v E c r)   = ((k, v), c, r)
+minKeyValTree (Node k v l c r)     = let
+                                        (keyval, centre, modifiedTree) = minKeyValTree l
+                                     in
+                                        (keyval, centre, Node k v modifiedTree c r) 
+
+
 delete :: Ord k => [k] -> TTree k v -> TTree k v
 delete [] _ = error "Error: keys must be non-empty"
 delete _ E = E
 
-delete lx@(x:xs) lf@(Leaf k v) 
+delete (x:xs) lf@(Leaf k v) 
           | x == k  = if null xs then E else lf
           | otherwise = lf 
 
 delete lx@(x:xs) (Node k v l c r) 
-          | x == k   = if null xs then (Node k Nothing l c r)
+          | x == k   = if null xs then balance (replace (Node k Nothing l c r))
                                   else balance (Node k v l (delete xs c) r) 
           | x > k    = balance (Node k v l c (delete lx r))
           | x < k    = balance (Node k v (delete lx l) c r)
 
           where
+
+               balance :: Ord k => TTree k v -> TTree k v
                balance (Node k (Just v) E E E) = Leaf k v
                balance (Node k Nothing E E E)  = E 
                balance t                       = t
 
+               replace :: Ord k => TTree k v -> TTree k v
+               replace E                = E
+               replace (Node _ _ l E E) = l
+               replace (Node _ _ E E r) = r
+               replace (Node _ _ l E r) = let
+                                             ((minKey, minVal), minCentre, modRight) = minKeyValTree r
+                                          in  
+                                             Node minKey minVal l minCentre modRight
+               replace t                = t
+               -- Si llegue aca, el centro no era E, entonces no hace falta reemplazar el nodo
+
+
+
+-- observaciones
+-- Los nodos SIN centro y SIN valor no tienen sentido, en la recursion podemos asumir que no estan
+-- Cada arbol si le ignoramos los centros de cada nodo es un BST. Esto nos facilita que
+-- no importa el origen realmente, yo cualquiera de los nodos podria ser raiz pues siempre van a respetar
+-- la invariante. Solo hay que encontrar un nodo que tenga sentido que sea raiz.
 
 -- d) Devuelve una lista ordenada con las claves del mismo
 keys :: TTree k v -> [[k]]
